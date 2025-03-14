d3ff326c98785ed74704de7ec1094be52ec45ad7
woollim <137277221+woollim23@users.noreply.github.com>
2025-03-14 22:51:57 +0900

    update: 계산기 함수 추가, 무한 계산 while문 추가

--- CPP_Calculator/CPP_Calculator/main.cpp
+++ CPP_Calculator/CPP_Calculator/main.cpp
@@ -2,7 +2,28 @@
 #include <string>
 #include <cmath>
 
+using namespace std;
+
+int calculator(int a, int b, char sign)
+{
+	if (sign == '+')
+		return a + b;
+	else if (sign == '-')
+		return a - b;
+	else if (sign == '*')
+		return a * b;
+	else if (sign == '/')
+		return a / b;
+}
+
 void main()
 {
+	while (1)
+	{
+		int a, b;
+		char sign;
+		cin >> a >> b >> sign;
 
+		cout << "°á°ú : " << calculator(a, b, sign) << endl;
+	}
 }
